1063e82008a9c9d399152a4e62e33b73a2020cb7
Aleksei Nikulshin <an1004@mail.ru>
2020-04-09 18:55:13 +0300

    Code changed

--- Code.py
+++ Code.py
@@ -1,32 +1,67 @@
+# задания 1 и 2 объединены
 
+def main():
 
-def check_exceptions():
-  print('Вы можете использовать следующие операции для двух положительных чисел: \nСложение\nВычитание\nУмножение\nДеление')
+  print \
+    ('Вы можете использовать следующие операции для двух положительных чисел: \nСложение\nВычитание\nУмножение\nДеление')
   print()
 
-  inp = input('Введите знак операции и положительные значения для двух чисел\n согласно правилам Польской нотации, разделяя каждый элемент ввода пробелом ')
+  inp = input \
+    ('Введите знак операции и положительные значения для двух чисел\n согласно правилам Польской нотации, разделяя каждый элемент пробелом\n\n ')
   inp = inp.split(' ')
+  # print(inp)
+  # print(type(inp))
 
-  result_input_list = list(filter(None, inp))
-  # print(result_input_list)
+
+  inp_str = list(filter(None, inp))
+  # print(inp_str)
+  # print(len(inp_str))
+
+
+  stack = 0
 
   try:
-    assert result_input_list[0] in ['+', '-', '*', '/'], 'первая операция не находится в списке доступных операций'
-  except AssertionError:
-    print('\nПервая операция не находится в списке доступных операций')
-
-  try:
-    if not isinstance(result_input_list[1:2], int):
-      a = int(result_input_list[1])
-      b = int(result_input_list[2])
-    c = a / b
-  except ZeroDivisionError:
-    print('\nДеление на ноль недопустимо')
+    assert inp_str[0] in ['+', '-', '*', '/'], 'первая операция не находится в списке доступных операций'
+    if int(inp_str[1]) < 0 or int(inp_str[2]) < 0:
+      print('Введенные Вами данные содержат отрицательные значения, что противоречит начальным условиям\n')
+      main()
+    if not isinstance(inp_str[1:2], int):
+      a = int(inp_str[1])
+      b = int(inp_str[2])
+    # c = a / b
   except ValueError:
     print('\nВведенные Вами значения не являются числами')
   except IndexError:
     print('\nКоличество аргументов, введенных Вами  недостаточно')
-  if len(result_input_list) > 3:
+  except AssertionError:
+    print('\nОперация не находится в списке доступных операций')
+  if len(inp_str) > 3:
     print('\nКоличество аргументов, введенных Вами  избыточно')
 
-check_exceptions()
+  try:
+    for i in inp_str:
+      if i == '-':
+        stack = int(inp_str[1]) - int(inp_str[2])
+        print(f'\nРезультат равен {stack}')
+        break
+      elif i == '*':
+        stack = int(inp_str[1]) * int(inp_str[2])
+        print(f'\nРезультат равен {stack}')
+        break
+      elif i == '/':
+        stack = int(inp_str[1]) / int(inp_str[2])
+        print(f'\nРезультат равен {stack}')
+        break
+      elif i == '+':
+        stack = int(inp_str[1]) + int(inp_str[2])
+        print(f'\nРезультат равен {stack}')
+        break
+  except ZeroDivisionError:
+    print('\nДеление на ноль недопустимо')
+
+
+
+main()
+
+
+
